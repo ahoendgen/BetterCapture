a349d97a8f3e61296ca95849de82c74c58001d0d
André Hoendgen <hello@andre-hoendgen.de>
2026-02-28 23:04:03 +0100

    chore: update sparkle feed url and remove website link

--- SuperCapture/View/SettingsView.swift
+++ SuperCapture/View/SettingsView.swift
@@ -381,10 +381,6 @@ struct AboutSection: View {
         Section("About") {
             LabeledContent("Version", value: "v\(appVersion) (supercharged)")
 
-            LabeledContent("Website") {
-                Link("jsattler.github.io/SuperCapture", destination: URL(string: "https://jsattler.github.io/SuperCapture")!)
-            }
-
             LabeledContent("Source Code") {
                 Link("github.com/ahoendgen/BetterCapture", destination: URL(string: "https://github.com/ahoendgen/BetterCapture")!)
             }
